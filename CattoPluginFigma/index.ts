20f7cecc1fd0a918076793e0e9dd9a9b87f3ed87
Catto <catto@live.com>
2024-12-11 18:41:28 -0500

    xmlbuilder small example

--- CattoPluginFigma/index.ts
+++ CattoPluginFigma/index.ts
@@ -1,3 +1,17 @@
 
 
+const { create } = require('xmlbuilder2');
+
 console.log("inside index");
+
+const root = create({ version: '1.0' })
+  .ele('root', { att: 'val' })
+    .ele('foo')
+      .ele('bar').txt('foobar').up()
+    .up()
+    .ele('baz').up()
+  .up();
+
+// convert the XML tree to string
+const xml = root.end({ prettyPrint: true });
+console.log(xml);
